e1282cbdfb93ac1d1f8e619c8a3bb89d7c86c2a6
Carson Tolleshaug <carson.tolleshaug@gmail.com>
2021-01-18 23:02:53 -0800

    initial commit

---
+++ CLImber/Configuration/ClimberConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CLImber.Configuration
+{
+    public class ClimberConfig
+    {
+        public string Shell { get; set; }
+        public ICollection<EndpointConfig> Endpoints { get; set; }
+    }
+}
---
+++ CLImber/Configuration/EndpointConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace CLImber.Configuration
+{
+    public class EndpointConfig
+    {
+        public string Name { get; set; }
+        public string Pattern { get; set; }
+        public string Run { get; set; }
+    }
+}
